39f4aadb10afd4dba3a4770697aab8395655295e
DanL12186 <danlrcn@hotmail.com>
2019-06-24 11:39:14 -0400

    create appointments table

--- db/migrate/20190620210807_create_appointments.rb
+++ db/migrate/20190620210807_create_appointments.rb
@@ -1,6 +1,14 @@
 class CreateAppointments < ActiveRecord::Migration[5.2]
   def change
-    create_table :appointments do |t|
+    create_table :appointments do | t |
+      t.belongs_to :user
+
+      t.datetime :date, null: false
+      t.string :category
+      t.string :notes
+
+      t.boolean :attended, default: nil
+      t.boolean :paid, default: nil
     end
   end
 end
