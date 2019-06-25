87d92fff3089a81a7a9ba6b3e3e5992ed51305c6
DanL12186 <danlrcn@hotmail.com>
2019-06-25 17:11:16 -0400

    update appointments schema/add test files

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,13 @@ ActiveRecord::Schema.define(version: 2019_06_20_210807) do
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "date", null: false
+    t.string "category"
+    t.string "notes"
+    t.boolean "attended"
+    t.boolean "paid"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
