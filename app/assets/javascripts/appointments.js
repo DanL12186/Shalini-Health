429c786631440dec1eac2138d2fb04bc9d891691
DanL12186 <danlrcn@hotmail.com>
2019-06-26 20:23:43 -0400

    pretty up a bit

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,5 +1,5 @@
 document.addEventListener('turbolinks:load', function() {
-  const authToken = document.querySelectorAll('head meta')[1].content
+  const authToken = document.querySelectorAll('head [name=csrf-token]')[0].content
   
   //String as "Month Year", e.g. "June 2019"
   const monthAndYear = document.querySelector('.calendar-title'),
@@ -16,17 +16,17 @@ document.addEventListener('turbolinks:load', function() {
           October: 10,
           November: 11,
           December: 12
-        }, 
-        timeSlots = [ '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00' ]
+        },
+        timeSlots = [ '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00' ];
             
   //shalani's only working fridays, so we loop over these (wday-5)
-  document.querySelectorAll('.day.wday-5.future').forEach(function(friday) {
-    friday.addEventListener('click', function() {
+  document.querySelectorAll('.day.wday-5.future').forEach(friday => {
+    friday.addEventListener('click', event => {
       friday.style.backgroundColor = 'red';
 
       const [month, year] = monthAndYear.innerText.split(' '),
             monthNum  = monthsToNumbers[month],
-            day       = this.innerText,
+            day       = event.target.innerText,
             url       = `/appointments/get_availability?year=${year}&month=${monthNum}&day=${day}`,
             options = { 
               method: 'POST', 
