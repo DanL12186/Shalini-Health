dea809a23e19944f3b9a13bcd2dc131e88f36e03
DanL12186 <danlrcn@hotmail.com>
2019-06-27 13:51:28 -0400

    Render new calendar months in JS instead of loading page
    Click through months of the calendar without page refresh; update url
    in the address bar as a user clicks, replacing current history state
    with new url.

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,7 +1,6 @@
 document.addEventListener('turbolinks:load', function() {
+
   const authToken = document.querySelectorAll('head [name=csrf-token]')[0].content
-  
-  //String as "Month Year", e.g. "June 2019"
   const monthAndYear = document.querySelector('.calendar-title'),
         monthsToNumbers = {
           January: 1,
@@ -18,32 +17,74 @@ document.addEventListener('turbolinks:load', function() {
           December: 12
         },
         timeSlots = [ '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00' ];
-            
+
+  const updateURL = (currentURL, newURL) => history.replaceState(currentURL, null, newURL);
+
   //shalani's only working fridays, so we loop over these (wday-5)
-  document.querySelectorAll('.day.wday-5.future').forEach(friday => {
-    friday.addEventListener('click', event => {
-      friday.style.backgroundColor = 'red';
+  //current placeholder for a window of options that will pop up for the user to make an appt
+  const getAvailability = (url, options) => {
+    fetch(url, options)
+    .then(response => response.json())
+    .then(unavailableTimes => {
+      const unavailableHours =  unavailableTimes.map(date=> date.match(/\d{2}:\d{2}/)[0]),
+            availableTimes   =  timeSlots.filter(slot => !unavailableHours.includes(slot)),
+            calendar         =  document.querySelector('.simple-calendar'),
+            whiteBox         =  `<div class='whiteBox'>
+                                  ${availableTimes.map(time => `<div>${time}</div>`).join('\n')}
+                                </div>`
 
-      const [month, year] = monthAndYear.innerText.split(' '),
-            monthNum  = monthsToNumbers[month],
-            day       = event.target.innerText,
-            url       = `/appointments/get_availability?year=${year}&month=${monthNum}&day=${day}`,
-            options = { 
-              method: 'POST', 
-              credentials: 'same-origin',
-              headers: { 'X-CSRF-Token': authToken }
-            };
+      calendar.innerHTML = whiteBox
 
-      fetch(url, options)
-        .then(response => response.json())
-        .then(unavailableTimes => {
-          const unavailableHours = unavailableTimes.map(date=> date.match(/\d{2}:\d{2}/)[0]),
-                availableTimes   = timeSlots.filter(slot => !unavailableHours.includes(slot))
+      console.log('unavailable times are...', unavailableTimes)
+      console.log('available times are..', availableTimes)
+    });
+  };
 
-          console.log('unavailable times are...', unavailableTimes)
-          console.log('available times are..', availableTimes)
+  const addAppointmentListeners = () => {
+    document.querySelectorAll('.day.wday-5.future').forEach(friday => {
+      friday.addEventListener('click', event => {
+        friday.style.backgroundColor = 'red';
+
+        const [month, year] = monthAndYear.innerText.split(' '),
+              monthNum  = monthsToNumbers[month],
+              day       = event.target.innerText,
+              url       = `/appointments/get_availability?year=${year}&month=${monthNum}&day=${day}`,
+              options = { 
+                method: 'POST', 
+                credentials: 'same-origin',
+                headers: { 'X-CSRF-Token': authToken }
+              };
+
+        getAvailability(url, options)
       });
     });
-  });
+  };
 
+  const loadNewCalendar = (response, event) => {
+    const newCalendar = response.body.querySelector('.simple-calendar'),
+          oldCalendar = document.querySelector('.simple-calendar');
+
+    oldCalendar.innerHTML = newCalendar.innerHTML;
+
+    addAppointmentListeners();
+    addNextPrevButtonListeners()
+    updateURL(window.location.href, event.target.href);
+  };
+
+  const addNextPrevButtonListeners = () => {
+    const nextAndPrevButtons = document.querySelectorAll('.calendar-heading a');
+    nextAndPrevButtons.forEach(calendarLink => {
+      calendarLink.addEventListener('click', event => {
+        event.preventDefault()
+        Rails.ajax({
+          type: 'GET',
+          url: event.target.href,
+          success: response => loadNewCalendar(response, event)
+        });
+      })
+    })
+  };
+
+  addNextPrevButtonListeners()
+  addAppointmentListeners()
 })
