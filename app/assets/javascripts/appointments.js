f8f417fa2936b695f7bdad1d5ad5f6ad36b3a593
DanL12186 <danlrcn@hotmail.com>
2019-06-28 16:52:53 -0400

    Fixed some issues and refactored

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,9 +1,6 @@
 document.addEventListener('turbolinks:load', function() {
   const authToken = document.querySelectorAll('head [name=csrf-token]')[0].content;
-
-  const modal           = document.getElementById("myModal"),
-        monthAndYear    = document.querySelector('.calendar-title'),
-        monthsToNumbers = {
+  const monthsToNumbers = {
           January: 1,
           February: 2,
           March: 3, 
@@ -27,19 +24,13 @@ document.addEventListener('turbolinks:load', function() {
           '15:00',
           '16:00',
           '17:00'
-        ];
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target === modal) {
+        ],
+        modal = document.getElementById("myModal");
         
-        //clear modal
-        document.querySelector('.modal-content').innerHTML = ''
-        modal.style.display = "none";
-      }
-    }
 
-  const updateURL = (currentURL, newURL) => history.replaceState(currentURL, null, newURL);
+  const updateURL = (currentURL, newURL) => {
+    history.replaceState(currentURL, null, newURL);
+  };
 
   const militaryToStandardTime = time => {
     if (time === '12:00') {
@@ -51,6 +42,16 @@ document.addEventListener('turbolinks:load', function() {
     }
   }
 
+  // When the user clicks anywhere outside of the modal, close it
+  window.addEventListener('click', event => {
+    if (event.target === modal) {
+      
+      //clear modal
+      document.querySelector('.modal-content').innerHTML = ''
+      modal.style.display = "none";
+    }
+  });
+
   //current placeholder for a window of options that will pop up for the user to make an appt
   const getAvailability = (url, options, month, day, year) => {
     fetch(url, options)
@@ -61,39 +62,52 @@ document.addEventListener('turbolinks:load', function() {
             standardTimes    =  availableTimes.map(time => militaryToStandardTime(time)),
             modalContent     =  document.querySelector('.modal-content');
 
-      const createRadioButton = time => `<input type="radio" id="${time}" name="timeSelect" value="${time}">
+      const createRadioButton = time => `<input type="radio" id="${time}" name="hour" value="${time}">
                                         <label for="${time}">${time}</label>`;
+
+      const formPayload = `authenticity_token=${authToken}&day=${day}&month=${monthsToNumbers[month]}&year=${year}`
 
       modalContent.innerHTML = `<span class="close">&times;</span>
                                 <h2 class='date-title'> Available Times for Friday, ${month} ${day}, ${year}: </h2>
-                                <form class='selectAppointment'>
+                                <form class='selectAppointment' method='POST' action='/appointments?${formPayload}'>
                                   <div id='options'></div>
                                 </form>`
 
-      const form = document.querySelector('.selectAppointment')
-      
-      standardTimes.forEach(time => {
-        form.innerHTML += `<p> ${createRadioButton(time, time)} </p>`
-      });
+      const form         = document.querySelector('.selectAppointment'),
+            radioButtons = standardTimes.map(time => `<p> ${createRadioButton(time, time)} </p>`).join(''),
+            submitButton = `<button input='submit'>Submit</button>`;
 
-      form.innerHTML += `<button input='submit'>Submit</button>`
-      document.querySelectorAll('.close').forEach(thing=> {
-        thing.addEventListener('click', function() {
+      form.innerHTML += radioButtons + submitButton
+
+      document.querySelector('.close').addEventListener('click', () => {
           modal.style.display = "none";
-        })
       });
     });
   };
-  
-  //shalani's only working fridays, so we only loop over these (wday-5)
+
+  //replace current calendar with new one
+  const loadNewCalendar = (response, event) => {
+    const oldCalendar = document.querySelector('.simple-calendar'),
+          newCalendar = response.body.querySelector('.simple-calendar');
+
+    oldCalendar.innerHTML = newCalendar.innerHTML;
+
+    addCalendarListeners();
+    addNextPrevButtonListeners()
+    updateURL(window.location.href, event.target.href);
+  };
+
+  //add event listeners for users clicking on clickable dates (future fridays)
+  //All available times from the selected day will be displayed
   const addCalendarListeners = () => {
     const availableDays = document.querySelectorAll('.day.wday-5.future')
+
     availableDays.forEach(friday => {
       friday.addEventListener('click', event => {
-        friday.style.backgroundColor = 'red';
         modal.style.display = "block";
 
-        const [month, year] = monthAndYear.innerText.split(' '),
+        const monthAndYear  = document.querySelector('.calendar-title'),
+             [month, year] = monthAndYear.innerText.split(' '),
               monthNum      = monthsToNumbers[month],
               day           = event.target.innerText,
               url           = `/appointments/get_availability?year=${year}&month=${monthNum}&day=${day}`,
@@ -108,17 +122,7 @@ document.addEventListener('turbolinks:load', function() {
     });
   };
 
-  const loadNewCalendar = (response, event) => {
-    const oldCalendar = document.querySelector('.simple-calendar'),
-          newCalendar = response.body.querySelector('.simple-calendar');
-
-    oldCalendar.innerHTML = newCalendar.innerHTML;
-
-    addCalendarListeners();
-    addNextPrevButtonListeners()
-    updateURL(window.location.href, event.target.href);
-  };
-
+  //load new calendar month when user clicks to another month
   const addNextPrevButtonListeners = () => {
     const nextAndPrevButtons = document.querySelectorAll('.calendar-heading a');
     nextAndPrevButtons.forEach(calendarLink => {
