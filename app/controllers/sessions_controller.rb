1abf5928b9a8e430ed0dd0da6da311e76519e13e
DanL12186 <danlrcn@hotmail.com>
2019-07-01 18:11:50 -0400

    fixed mistake with name (should be email)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: session_params[:name])
+    @user = User.find_by(email: session_params[:email])
     session[:user_id] = @user.id if @user && @user.authenticate(session_params[:password])
 
     if !!session[:user_id]
       redirect_to root_path
     else
-      @user = User.new(name: session_params[:name])
+      @user = User.new(email: session_params[:email])
       flash[:error] = "Username and password don't match"
       render :new
     end
@@ -26,7 +26,7 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:user).permit(:password, :name, :uid)
+    params.require(:user).permit(:password, :email, :uid)
   end
 
 end
