513c4135a003bcf81141e693b9bb5a76b2b5a16f
DanL12186 <danlrcn@hotmail.com>
2019-07-02 12:20:09 -0400

    set up emailing to notify users when they've...
    successfully signed up to the site. Currently using ActionMailer via
    Google, which is not as secure; should probably migrate to Mailgun
    later.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save ? (session[:user_id] = @user.id; redirect_to root_path) : (render :new)
+
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.with(user: @user).welcome_email.deliver_later
+
+        session[:user_id] = @user.id if @user.save
+ 
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
