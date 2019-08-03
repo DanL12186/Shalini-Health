893346e14d58dab7d0268416dc48797f2e9f5a8b
DanL12186 <daniel.lempesis@gmail.com>
2019-08-03 18:14:26 -0400

    Passed responsibility to hiding header/footer on recipes page to javascript
    This way it actually only hides/shows it on the header/footer page at
    under 1000px width. Perhaps later use ifMobileDevice() instead

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,2 +1,27 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('turbolinks:load', () => {
+
+  const hideHeaderAndFooter = () => {
+      document.querySelector('.bg-head').classList.add('hide')
+      document.querySelector('.bg-foot').classList.add('hide')
+  }
+
+  const showHeaderAndFooter = () => {
+    document.querySelector('.bg-head').classList.remove('hide')
+    document.querySelector('.bg-foot').classList.remove('hide')
+  }
+
+  if (document.querySelector("#recipe-page")) {
+    if (window.innerWidth <= 1000) {
+      hideHeaderAndFooter()
+    }
+    window.addEventListener('resize', () => {
+      if (window.innerWidth <= 1000) {
+        hideHeaderAndFooter()
+      } else {
+        showHeaderAndFooter()
+      }
+    }) 
+  }
+})
