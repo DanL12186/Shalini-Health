1c540f1aa62d1d4bb447e9eacda75dd8873ecf19
DanL12186 <daniel.lempesis@gmail.com>
2019-08-06 16:51:09 -0400

    Refactored some and fixed a bug where event listener stayed attached on the next page

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,27 +1,39 @@
-// Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 document.addEventListener('turbolinks:load', () => {
 
-  const hideHeaderAndFooter = () => {
-    document.querySelector('.bg-head').classList.add('hide')
-    document.querySelector('.bg-foot').classList.add('hide')
+  const hideOrShowHeaderAndFooter = hideOrShow => {
+    if (hideOrShow === 'hide') {
+      document.querySelector('.bg-head').classList.add('hide')
+      document.querySelector('.bg-foot').classList.add('hide')
+    } else {
+      document.querySelector('.bg-head').classList.remove('hide')
+      document.querySelector('.bg-foot').classList.remove('hide')
+    }
   }
 
-  const showHeaderAndFooter = () => {
-    document.querySelector('.bg-head').classList.remove('hide')
-    document.querySelector('.bg-foot').classList.remove('hide')
+  //hide/show the footer and header if screen width crosses 1000px threshold
+  const widthListener = () => {
+    if (window.innerWidth <= 1000) {
+      hideOrShowHeaderAndFooter('hide')
+    } else {
+      hideOrShowHeaderAndFooter('show')
+    }
   }
 
+  //execute only on recipe pages
+  //immediately hide header/footer if page <= 1000 px, and listen for resizing
   if (document.querySelector("#recipe-page")) {
     if (window.innerWidth <= 1000) {
-      hideHeaderAndFooter()
+      hideOrShowHeaderAndFooter('hide')
     }
-    window.addEventListener('resize', () => {
-      if (window.innerWidth <= 1000) {
-        hideHeaderAndFooter()
-      } else {
-        showHeaderAndFooter()
-      }
-    }) 
+
+    window.addEventListener('resize', widthListener)
+
+    //remove event listener upon leaving the page
+    document.addEventListener('turbolinks:before-visit', function() {
+      window.removeEventListener('resize', widthListener);
+    })
+
   }
 })
+
