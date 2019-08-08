8910ad1e70c9cf9c3ba07e55e5d16e2fbeb5f13c
DanL12186 <daniel.lempesis@gmail.com>
2019-08-08 15:35:02 -0400

    Added pages for counseling and cooking classes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def about
   end
+
+  def nutrition_counseling
+  end
+
+  def cooking_classes
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'application#home'
+
+  get 'static_pages/about'
+  get 'static_pages/nutrition_counseling'
+  get 'static_pages/cooking_classes'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
